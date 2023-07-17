d3ca59008f75933b55f665ae1a15879c4930daf8
Jarod Lam <jarlam@deloitte.com.au>
2023-07-17 14:54:05 +1000

    Static donut

--- src/Donut.jl
+++ src/Donut.jl
@@ -1,6 +1,5 @@
 module Donut
 
-# import Plots
 using GLMakie
 
 export donut
@@ -20,90 +19,41 @@ function donut(
         K2::Float64=5.0,
         R1::Float64=1.0,
         R2::Float64=2.0,
+        nframes::Integer=50
     )
     # Define the donut
-    donut_surface(θ, ϕ, A, B) = [
-        (R2+R1*cos(θ))*(cos(B)*cos(ϕ)+sin(A)*sin(B)*sin(ϕ))-R1*cos(A)*sin(B)*sin(θ);
-        (R2+R1*cos(θ))*(sin(B)*cos(ϕ)-sin(A)*cos(B)*sin(ϕ))+R1*cos(A)*sin(B)*sin(θ);
-        cos(A)*(R2+R1*cos(θ))*sin(ϕ)+R1*sin(A)*sin(θ)
-    ]
+    fx(θ, ϕ, A, B) = (R2+R1*cos(θ))*(cos(B)*cos(ϕ)+sin(A)*sin(B)*sin(ϕ))-R1*cos(A)*sin(B)*sin(θ)
+    fy(θ, ϕ, A, B) = (R2+R1*cos(θ))*(sin(B)*cos(ϕ)-sin(A)*cos(B)*sin(ϕ))+R1*cos(A)*cos(B)*sin(θ)
+    fz(θ, ϕ, A, B) = cos(A)*(R2+R1*cos(θ))*sin(ϕ)+R1*sin(A)*sin(θ)
 
     # Make the donut
-    theta = range(0, stop=2pi, length=100)
-    phi = range(0, stop=2pi, length=100)
-    A = 0
-    B = 0
-    points = donut_surface.(theta, phi, A, B)
+    Ts = range(0, stop=2pi, length=100)
+    Ps = range(0, stop=2pi, length=100)
+    As = range(0, stop=2pi, length=nframes)
+    Bs = range(0, stop=2pi, length=nframes)
+    x = [[fx(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
+    y = [[fy(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
+    z = [[fz(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
 
     # Show the donut
-    fig = Figure()
-    ax = LScene(fig)
-    torus = wireframe!(
-        ax, 
-        [p[1] for p in points], 
-        [p[2] for p in points], 
-        [p[3] for p in points]; 
-        # colormap=:plasma, 
-        # shading=false
+    fig = Figure(resolution = (1200, 800))
+    ax = Axis3(
+        fig; 
+        aspect=(1, 1, 1), 
+        limits=(-3, 3, -3, 3, -3, 3), 
+        viewmode=:fit
     )
+    torus = surface!(
+        ax, 
+        x[30], y[30], z[30], 
+        colormap=:heat
+    )
+    hidedecorations!(ax)
+    hidespines!(ax)
+    fig[1, 1] = ax
+    fig
 
     return fig
-end
-
-function example()
-    # U = LinRange(-pi, pi, 100)
-    # V = LinRange(-pi, pi, 20)
-    # x1 = [cos(u) + 0.5 * cos(u) * cos(v) for u in U, v in V]
-    # y1 = [sin(u) + 0.5 * sin(u) * cos(v) for u in U, v in V]
-    # z1 = [0.5 * sin(v) for u in U, v in V]
-    # x2 = [1 + cos(u) + 0.5 * cos(u) * cos(v) for u in U, v in V]
-    # y2 = [0.5 * sin(v) for u in U, v in V]
-    # z2 = [sin(u) + 0.5 * sin(u) * cos(v) for u in U, v in V]
-
-    R1=1.0
-    R2=2.0
-    donut_surface(θ, ϕ, A, B) = [
-        (R2+R1*cos(θ))*(cos(B)*cos(ϕ)+sin(A)*sin(B)*sin(ϕ))-R1*cos(A)*sin(B)*sin(θ);
-        (R2+R1*cos(θ))*(sin(B)*cos(ϕ)-sin(A)*cos(B)*sin(ϕ))+R1*cos(A)*sin(B)*sin(θ);
-        cos(A)*(R2+R1*cos(θ))*sin(ϕ)+R1*sin(A)*sin(θ)
-    ]
-    theta = range(0, stop=2pi, length=100)
-    phi = range(0, stop=2pi, length=100)
-    A = 0
-    B = 0
-    points = [donut_surface(t, p, A, B) for t in theta for p in phi]
-    x1 = [p[1] for p in points]
-    y1 = [p[2] for p in points]
-    z1 = [p[3] for p in points]
-
-    fig = Figure(resolution = (1200, 800))
-    ax = LScene(fig, show_axis = true)
-    tori1 = surface!(ax, x1, y1, z1; colormap = :viridis, shading = false,
-        transparency = true)
-    # tori2 = surface!(ax, x2, y2, z2; colormap = :plasma, shading = false,
-    #     transparency = false)
-    # wireframe!(ax, x1, y1, z1; linewidth = 0.5, transparency = true)
-
-    # axis = ax.scene[OldAxis]
-    # axis[:names, :axisnames] = ("x", "y", "z")
-    # tstyle = axis[:names] #  get the nested attributes and work directly with them
-
-    # tstyle[:fontsize] = 15
-    # tstyle[:textcolor] = (:red, :green, :black)
-    # tstyle[:font] = "helvetica"
-    # tstyle[:gap] = 10
-    # axis[:ticks][:textcolor] = :black
-    # axis[:ticks][:fontsize] = 10
-    # cbar1 = Colorbar(fig, tori1, label = "z", width = 25, ticklabelsize = 20,
-    #     labelsize = 20, ticksize = 25, tickalign = 1, height = Relative(0.5))
-    # cbar2 = Colorbar(fig, tori2, label = "z", width = 25, flipaxis = false,
-    #     labelsize = 20, ticklabelsize = 20, ticksize = 25, tickalign = 1,
-    #     height = Relative(0.5))
-    # fig[1, 2] = ax
-    # fig[1, 3] = cbar1
-    # fig[1, 1] = cbar2
-    # colgap!(fig.layout, 2)
-    fig
 end
 
 end # module

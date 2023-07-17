d0fdb89e1d90317b413fbc793026aa6a8d5c2909
Jarod Lam <jarlam@deloitte.com.au>
2023-07-17 16:28:46 +1000

    Spinning donut

---
+++ README.md
@@ -0,0 +1,10 @@
+# Donut.jl
+A Julia implementation of [donut.c](https://www.a1k0n.net/2006/09/15/obfuscated-c-donut.html) using Makie for rendering.
+
+![spinning donut](donut.gif)
+
+## Usage
+```julia
+julia> using Donut
+julia> donut()
+```
--- src/Donut.jl
+++ src/Donut.jl
@@ -5,53 +5,83 @@ using GLMakie
 export donut
 
 """
-donut
+    donut()
 
-# Arguments
-- `K1::Float64`: Distance from eye to screen
-- `K2::Float64`: Distance from screen to donut
-- `R1::Float64`: Donut thickness radius
-- `R2::Float64`: Donut hole radius
+Animate a 3D spinning donut.
 
+# Keyword arguments
+- `outfile::AbstractString`: Output to a file instead of displaying on screen. 
+  Supports `.gif`, `.mp4`, `.mkv`, and `.webm`. 
+  See https://docs.makie.org/stable/documentation/animation/#file_formats.
+- `R1::Real`: Donut thickness radius
+- `R2::Real`: Donut hole radius
+- `xspinperiod::Real`: Donut spin period about the x-axis in seconds
+- `zspinperiod::Real`: Donut spin period about the z-axis in seconds
+- `duration::Real`: Duration of the animation
+- `framerate::Real`: Framerate of the animation
+- `resolution::Tuple{Int,Int}`: Resolution of the animation in pixels
 """
-function donut(
-        K1::Float64=5.0,
-        K2::Float64=5.0,
-        R1::Float64=1.0,
-        R2::Float64=2.0,
-        nframes::Integer=50
+function donut(;
+        outfile::Union{Nothing,AbstractString}=nothing,
+        R1::Real=1.0,
+        R2::Real=2.0,
+        xspinperiod::Real=2.0,
+        zspinperiod::Real=5.0,
+        duration::Real=10.0,
+        framerate::Real=60,
+        resolution::Tuple{Integer,Integer}=(800, 800)
     )
-    # Define the donut
+    # Define the donut surface
     fx(θ, ϕ, A, B) = (R2+R1*cos(θ))*(cos(B)*cos(ϕ)+sin(A)*sin(B)*sin(ϕ))-R1*cos(A)*sin(B)*sin(θ)
     fy(θ, ϕ, A, B) = (R2+R1*cos(θ))*(sin(B)*cos(ϕ)-sin(A)*cos(B)*sin(ϕ))+R1*cos(A)*cos(B)*sin(θ)
     fz(θ, ϕ, A, B) = cos(A)*(R2+R1*cos(θ))*sin(ϕ)+R1*sin(A)*sin(θ)
 
-    # Make the donut
-    Ts = range(0, stop=2pi, length=100)
-    Ps = range(0, stop=2pi, length=100)
-    As = range(0, stop=2pi, length=nframes)
-    Bs = range(0, stop=2pi, length=nframes)
-    x = [[fx(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
-    y = [[fy(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
-    z = [[fz(t, p, As[i], Bs[i]) for t in Ts, p in Ps] for i in 1:nframes]
+    # Define the donut spin
+    fA(t) = 2pi / xspinperiod * t
+    fB(t) = 2pi / zspinperiod * t
 
-    # Show the donut
-    fig = Figure(resolution = (1200, 800))
+    # Make the donut
+    time = Observable(0.0)
+    θs = range(0, stop=2pi, length=100)
+    ϕs = range(0, stop=2pi, length=50)
+    fx(t) = [fx(θ, ϕ, fA(t), fB(t)) for θ in θs, ϕ in ϕs]
+    fy(t) = [fy(θ, ϕ, fA(t), fB(t)) for θ in θs, ϕ in ϕs]
+    fz(t) = [fz(θ, ϕ, fA(t), fB(t)) for θ in θs, ϕ in ϕs]
+    xs = @lift fx($time)
+    ys = @lift fy($time)
+    zs = @lift fz($time)
+
+    # Plot the donut
+    fig = Figure(resolution=resolution)
     ax = Axis3(
         fig; 
         aspect=(1, 1, 1), 
         limits=(-3, 3, -3, 3, -3, 3), 
-        viewmode=:fit
     )
-    torus = surface!(
+    surface!(
         ax, 
-        x[30], y[30], z[30], 
+        xs, ys, zs;
         colormap=:heat
     )
     hidedecorations!(ax)
     hidespines!(ax)
     fig[1, 1] = ax
-    fig
+
+    # Spin the donut
+    timestamps = range(0, stop=duration, step=1/framerate)
+    if !isnothing(outfile)
+        # Save the donut
+        record(fig, outfile, timestamps; framerate=framerate) do t
+            time[] = t
+        end
+    else
+        # Show the donut
+        display(fig)
+        for t in timestamps
+            time[] = t
+            sleep(1 / framerate)
+        end
+    end
 
     return fig
 end
